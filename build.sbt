67dfaa7355142652312baf5ceddda6243438fde0
Steve Phelps <sphelps@sphelps.net>
2022-04-26 15:27:57 +0100

    unit tests for TxWatchActor

--- build.sbt
+++ build.sbt
@@ -8,12 +8,17 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.13.8"
 
 libraryDependencies += guice
-libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
 libraryDependencies += "org.bitcoinj" % "bitcoinj-core" % "0.16.1"
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.11"
 libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "2.30.0"
-libraryDependencies += "org.awaitility" % "awaitility" % "4.0.1" % Test
-libraryDependencies += "org.scalamock" %% "scalamock" % "5.1.0" % Test
+
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test
+libraryDependencies += "org.awaitility" % "awaitility" % "4.2.0" % Test
+libraryDependencies += "org.scalamock" %% "scalamock" % "5.2.0" % Test
+val AkkaVersion = "2.6.19"
+libraryDependencies += "com.typesafe.akka" %% "akka-testkit" % AkkaVersion % Test
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.11" % Test
+
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "com.mesonomics.controllers._"
 
--- test/TxWatchActorTests.scala
+++ test/TxWatchActorTests.scala
@@ -1,12 +1,65 @@
+import actors.TxWatchActor
+import actors.TxWatchActor.{Auth, TxUpdate}
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.pattern.ask
+import akka.testkit.{ImplicitSender, TestKit}
+import com.github.nscala_time.time.Imports.DateTime
 import org.scalamock.scalatest.MockFactory
-import org.scalatestplus.play.PlaySpec
-import play.api.inject.guice.GuiceApplicationBuilder
+import org.scalatest.matchers.must.Matchers.convertToAnyMustWrapper
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import play.api.test.Helpers.defaultAwaitTimeout
+import services.{MemPoolWatcherService, User, UserManagerService}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
 
 //noinspection TypeAnnotation
-class TxWatchActorTests extends PlaySpec with MockFactory {
+class TxWatchActorTests extends TestKit(ActorSystem("MySpec"))
+  with Matchers
+  with AnyWordSpecLike
+  with MockFactory
+  with ImplicitSender {
+
+  object MockWebsocketActor {
+    def props(updates: ArrayBuffer[TxUpdate]) = Props(new MockWebsocketActor(updates))
+  }
+
+  class MockWebsocketActor(val updates: ArrayBuffer[TxUpdate]) extends Actor {
+
+    override def receive: Receive = {
+      case update: TxUpdate =>
+        updates += update
+      case _ =>
+        fail("unrecognized message format")
+    }
+  }
 
   def fixture = new {
-    val app = new GuiceApplicationBuilder().build()
+    val mockMemPoolWatcher = mock[MemPoolWatcherService]
+    (mockMemPoolWatcher.addListener _).expects(*)
+    val updates = ArrayBuffer[TxUpdate]()
+    def wsActor(implicit system: ActorSystem) = system.actorOf(MockWebsocketActor.props(updates))
+    def txWatchActor(mockUserManager: UserManagerService)(implicit system: ActorSystem) =
+      system.actorOf(TxWatchActor.props(wsActor, mockMemPoolWatcher, mockUserManager))
+  }
+
+  "TxWatchActor" should {
+
+    "provide updates when a valid user sends authentication" in {
+      val tx = TxUpdate("testHash", 10, DateTime.now(), isPending = true)
+      val mockUser = mock[User]
+      val mockUserManager = mock[UserManagerService]
+      (mockUser.filter _).expects(tx).returning(true)
+      (mockUserManager.authenticate _).expects("test").returning(mockUser)
+      implicit val system: ActorSystem = ActorSystem()
+      val f = fixture
+      val txWatchActor = f.txWatchActor(mockUserManager)
+      txWatchActor ! Auth("test", "test")
+      txWatchActor ! tx
+      f.updates.head mustBe tx
+    }
   }
 
 }
