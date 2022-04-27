cc71ff9012e0ba3c7e18c4fbb1e3a24a3076a3cc
Steve Phelps <sphelps@sphelps.net>
2022-04-27 13:14:45 +0100

    unit test to verify that the MemPoolWatcher generates a correct TxUpdate message when a broadcast is received from the bitcoin PeerGroup (#23)

--- test/TxWatchActorTests.scala
+++ test/TxWatchActorTests.scala
@@ -4,6 +4,9 @@ import akka.actor.{Actor, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import com.github.nscala_time.time.Imports.DateTime
 import com.google.common.util.concurrent.ListenableFuture
+import org.bitcoinj.core.{Address, Coin, Peer, Transaction, TransactionOutput}
+import org.bitcoinj.params.{MainNetParams, TestNet3Params}
+import org.scalamock.matchers.ArgCapture.{CaptureAll, CaptureOne}
 //import com.google.common.util.concurrent.ListenableFuture
 import org.bitcoinj.core.PeerGroup
 import org.bitcoinj.core.listeners.OnTransactionBroadcastListener
@@ -51,13 +54,50 @@ class TxWatchActorTests extends TestKit(ActorSystem("MySpec"))
 
   "MemPoolWatcher" should {
 
-    "send TxUpdate messages when transaction updates are received from the peer group" in {
+    "send the correct TxUpdate message  when a transaction update is received from " +
+      "the bitcoinj peer group" in {
+
+      val f = fixture
+      val updateCapture = CaptureOne[TxUpdate]()
+      (f.mockUser.filter _).expects(*).returning(true).once()
+      (f.mockUserManager.authenticate _).expects("test").returning(f.mockUser)
+      (f.mockWs.update _).expects(capture(updateCapture)).once()
+
+      val params = MainNetParams.get()
+      class MockPeerGroup extends PeerGroup(params)
+
       implicit val d = new Defaultable[ListenableFuture[_]] {
         override val default = null
       }
-      val mockPeerGroup = mock[PeerGroup]
-      (mockPeerGroup.addOnTransactionBroadcastListener(_: Executor, _: OnTransactionBroadcastListener)) expects (*, *)
+      val mockPeerGroup = mock[MockPeerGroup]
+      val c1 = CaptureAll[OnTransactionBroadcastListener]()
+      (mockPeerGroup.addOnTransactionBroadcastListener(_: OnTransactionBroadcastListener)).expects(capture(c1)).atLeastOnce()
       val memPoolWatcher = new MemPoolWatcher(new PeerGroupSelection() { val peerGroup = mockPeerGroup })
+      memPoolWatcher.addListener(f.txWatchActor)
+
+      val txWatchActor = system.actorOf(TxWatchActor.props(f.wsActor, memPoolWatcher, f.mockUserManager))
+      txWatchActor ! Auth("test", "test")
+      expectNoMessage()
+
+      val listener = c1.value
+      val transaction: Transaction = new Transaction(params)
+
+      val outputAddress1 = "1A5PFH8NdhLy1raKXKxFoqUgMAPUaqivqp"
+      val value1 = 100L
+      transaction.addOutput(Coin.valueOf(value1), Address.fromString(params, outputAddress1))
+
+      val outputAddress2 = "1G47mSr3oANXMafVrR8UC4pzV7FEAzo3r9"
+      val value2 = 200L
+      transaction.addOutput(Coin.valueOf(value2), Address.fromString(params, outputAddress2))
+
+      listener.onTransaction(null, transaction)
+      expectNoMessage()
+
+      val receivedTx = updateCapture.value
+      //noinspection ZeroIndexToHead
+      receivedTx.outputAddresses(0) shouldBe outputAddress1
+      receivedTx.outputAddresses(1) shouldBe outputAddress2
+      receivedTx.value shouldBe value1 + value2
     }
   }
 
