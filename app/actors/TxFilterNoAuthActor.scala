3e1ec01b2d5a43b88042a4735cfc8f777632f4d4
Steve Phelps <sphelps@sphelps.net>
2022-04-30 11:27:28 +0100

    added companion object props (#14)

--- app/actors/TxFilterNoAuthActor.scala
+++ app/actors/TxFilterNoAuthActor.scala
@@ -1,7 +1,12 @@
 package actors
 
-import akka.actor.ActorRef
+import akka.actor.{ActorRef, Props}
 import services.MemPoolWatcherService
+
+object TxFilterNoAuthActor {
+  def props(out: ActorRef, filter: TxUpdate => Boolean, memPoolWatcher: MemPoolWatcherService): Props =
+    Props(new TxFilterNoAuthActor(out, filter, memPoolWatcher))
+}
 
 class TxFilterNoAuthActor(val out: ActorRef, val filter: TxUpdate => Boolean,
                           memPoolWatcher: MemPoolWatcherService)
