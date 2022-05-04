fcb5f6269280a0e9b37463412cf25e0123c90940
Steve Phelps <sphelps@sphelps.net>
2022-05-04 23:13:40 +0100

    remove commented out code

--- app/actors/WebhookManagerActor.scala
+++ app/actors/WebhookManagerActor.scala
@@ -67,7 +67,6 @@ class WebhookManagerActor @Inject()(val memPoolWatcher: MemPoolWatcherService,
       if (webhooks contains uri) {
         val hook = webhooks(uri)
         logger.debug(s"hook = " + hook)
-//        val slackActor = context.actorOf(backendSelection.props(uri))
         val slackActor = injectedChild(create = childFactory(uri), name=encodeUrl(uri.toURL.toString))
         val actor = context.actorOf(TxFilterNoAuthActor.props(slackActor, _.value >= hook.threshold, memPoolWatcher))
         context.become(updated(webhooks, actors = actors + (uri -> actor)))
