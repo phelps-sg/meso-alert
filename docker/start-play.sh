d591b2cbfd35fc99d6ee467400f6a9ba5f10a21d
Steve Phelps <sphelps@sphelps.net>
2022-05-26 15:27:36 +0100

    obtain secrets from k8 mount point (#36)

--- docker/start-play.sh
+++ docker/start-play.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
-cd /root
 . "/root/.sdkman/bin/sdkman-init.sh"
+
+cd /root
+
+PLAY_KEY=$(cat /etc/secrets/play/secret)
+POSTGRES_PASSWORD=$(cat /etc/secrets/postgres/password)
+
+cat <<EOF > application-production.conf
+play.http.secret.key='${PLAY_KEY}'
+play.filters.disabled+=play.filters.hosts.AllowedHostsFilter
+
+meso-alert.db = {
+  connectionPool = "HikariCP" //use HikariCP for our connection pool
+  dataSourceClass = "org.postgresql.ds.PGSimpleDataSource"
+  properties = {
+    serverName = "meso-alert-postgres"
+    portNumber = "5432"
+    databaseName = "meso-alert"
+    user = "meso-alert"
+    password = "${POSTGRES_PASSWORD}"
+  }
+  numThreads = 12
+  queueSize = 50000
+}
+
+database.dispatcher {
+  executor = "thread-pool-executor"
+  throughput = 1
+  thread-pool-executor {
+    fixed-pool-size = 12
+  }
+}
+EOF
+
+unzip ./target/universal/meso-alert-1.0-SNAPSHOT.zip
+
 ./meso-alert-1.0-SNAPSHOT/bin/meso-alert -Dconfig.file=application-production.conf
