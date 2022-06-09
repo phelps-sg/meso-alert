7f02b3d4135f3f398a22ad0ece2f663f0dd2c2ec
Steve Phelps <sphelps@sphelps.net>
2022-06-09 11:25:17 +0100

    #52 dto class for slash command

--- app/controllers/SlackController.scala
+++ app/controllers/SlackController.scala
@@ -5,6 +5,11 @@ import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, R
 
 import javax.inject.Inject
 
+case class SlashCommandDto(token: String, teamId: String, teamDomain: String, enterpriseId: String,
+                           channelId: String, channelName: String, userId: String, userName: String,
+                           command: String, text: String, responseUrl: String, triggerId: String,
+                           apiAppId: String)
+
 class SlackController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
 
   private val logger = LoggerFactory.getLogger(classOf[SlackController])
