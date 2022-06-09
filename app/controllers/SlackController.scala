f4b2dd1aabc9fcba8002528e9084af39df875dd3
Steve Phelps <sphelps@sphelps.net>
2022-06-09 12:37:29 +0100

    #52 use unused json components

--- app/controllers/SlackController.scala
+++ app/controllers/SlackController.scala
@@ -1,21 +1,13 @@
 package controllers
 
 import org.slf4j.LoggerFactory
-import play.api.libs.json.OFormat
-import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Request}
-import play.api.libs.json._
+import play.api.mvc.{Action, BaseController, ControllerComponents}
+
 import javax.inject.Inject
 
 class SlackController @Inject() (val controllerComponents: ControllerComponents) extends BaseController {
 
   private val logger = LoggerFactory.getLogger(classOf[SlackController])
-
-  case class SlashCommandDto(token: String, teamId: String, teamDomain: String, enterpriseId: String,
-                             channelId: String, channelName: String, userId: String, userName: String,
-                             command: String, text: String, responseUrl: String, triggerId: String,
-                             apiAppId: String)
-
-  implicit val slashCommandJson: OFormat[SlashCommandDto] = Json.format[SlashCommandDto]
 
   def slashCommand: Action[Map[String, Seq[String]]] = Action(parse.formUrlEncoded) { request =>
     logger.debug("received slash command")
