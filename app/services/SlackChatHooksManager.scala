79ec00497078c4e41c65fb77dba65c6f599c662f
Steve Phelps <sphelps@sphelps.net>
2022-06-10 14:31:21 +0100

    #49 correct implementation of SlackChatHooksManager

---
+++ app/services/SlackChatHooksManager.scala
@@ -0,0 +1,18 @@
+package services
+
+import akka.actor.{ActorRef, ActorSystem}
+import com.google.inject.ImplementedBy
+import com.google.inject.name.Named
+import dao.{SlackChannel, SlackChatHook, SlackChatHookDao}
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+@ImplementedBy(classOf[SlackChatHooksManager])
+trait SlackChatHooksManagerService extends HooksManagerService[SlackChatHook, SlackChannel]
+
+@Singleton
+class SlackChatHooksManager @Inject()(val hookDao: SlackChatHookDao,
+                                @Named("slack-hooks-actor") val actor: ActorRef)
+                               (implicit val system: ActorSystem, val executionContext: ExecutionContext)
+  extends SlackChatHooksManagerService with HooksManager[SlackChannel, SlackChatHook]
