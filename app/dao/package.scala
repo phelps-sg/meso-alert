ba86ca78845e18615ec033b21c3f3948af0cafef
Steve Phelps <sphelps@sphelps.net>
2022-06-10 22:22:43 +0100

    refactored duplicate code into trait

--- app/dao/package.scala
+++ app/dao/package.scala
@@ -1,8 +1,11 @@
 import com.google.inject.{ImplementedBy, Inject, Singleton}
 import org.slf4j.{Logger, LoggerFactory}
 import slick.BtcPostgresProfile.api._
-import slick.{DatabaseExecutionContext, Tables}
+import slick.dbio.Effect
+import slick.{BtcPostgresProfile, DatabaseExecutionContext, Tables}
 import slick.jdbc.JdbcBackend.Database
+import slick.lifted.AbstractTable
+import slick.sql.FixedSqlAction
 
 import java.net.URI
 import scala.concurrent.Future
@@ -34,37 +37,20 @@ package object dao {
   @ImplementedBy(classOf[SlickSlackChatDao])
   trait SlackChatHookDao extends HookDao[SlackChannel, SlackChatHook]
 
-  @Singleton
-  class SlickWebhookDao @Inject() (val db: Database,
-                                   val databaseExecutionContext: DatabaseExecutionContext)
-    extends WebhookDao {
-
+  trait SlickHookDao[X, Y] {
+    val db: Database
+    val databaseExecutionContext: DatabaseExecutionContext
+    val logger: Logger
     implicit val ec: DatabaseExecutionContext = databaseExecutionContext
 
-    val logger: Logger = LoggerFactory.getLogger(classOf[SlickWebhookDao])
+    val table: TableQuery[_]
+    val lookupHookQuery: Y => Query[_, Y, Seq]
+    val lookupKeyQuery: X => Query[_, Y, Seq]
+    val insertHookQuery: Y => FixedSqlAction[Int, NoStream, Effect.Write]
 
-    def init(): Future[Unit] = db.run(Tables.webhooks.schema.createIfNotExists)
-
-    def insert(hook: Webhook): Future[Int] = {
-      for {
-        n: Int <- db.run(Tables.webhooks.filter(_.url === hook.uri.toString).size.result)
-        result <-
-          if (n > 0) {
-            throw DuplicateHookException(hook.uri)
-          } else {
-            db.run(Tables.webhooks += hook)
-          }
-      } yield result
-    }
-
-    def all(): Future[Seq[Webhook]] = db.run(Tables.webhooks.result)
-    def allKeys(): Future[Seq[URI]] = db.run(Tables.webhooks.map(_.url).result) map {
-      _.map(new URI(_))
-    }
-
-    def find(uri: URI): Future[Option[Webhook]] = {
+    def find(uri: X): Future[Option[Y]] = {
       logger.debug(s"Querying for ${uri.toString}")
-      db.run(Tables.webhooks.filter(_.url === uri.toString).result).map {
+      db.run(lookupKeyQuery(uri).result).map {
         case Seq(result) => Some(result)
         case Seq() => None
         case _ =>
@@ -72,43 +58,57 @@ package object dao {
       }
     }
 
-  }
-
-  @Singleton
-  class SlickSlackChatDao @Inject() (val db: Database,
-                                      val databaseExecutionContext: DatabaseExecutionContext)
-    extends SlackChatHookDao {
-
-    implicit val ec: DatabaseExecutionContext = databaseExecutionContext
-
-    val logger: Logger = LoggerFactory.getLogger(classOf[SlickWebhookDao])
-
-    def init(): Future[Unit] = db.run(Tables.slackChatHooks.schema.createIfNotExists)
-
-    def insert(hook: SlackChatHook): Future[Int] = {
+    def insert(hook: Y): Future[Int] = {
       for {
-        n: Int <- db.run(Tables.slackChatHooks.filter(_.channel_id === hook.channel.id).size.result)
+        n: Int <- db.run(lookupHookQuery(hook).size.result)
         result <-
           if (n > 0) {
-            throw DuplicateHookException(hook.channel)
+            throw DuplicateHookException(hook)
           } else {
-            db.run(Tables.slackChatHooks += hook)
+            db.run(insertHookQuery(hook))
           }
       } yield result
     }
 
+  }
+
+  //noinspection TypeAnnotation
+  @Singleton
+  class SlickWebhookDao @Inject() (val db: Database,
+                                   val databaseExecutionContext: DatabaseExecutionContext)
+    extends WebhookDao with SlickHookDao[URI, Webhook] {
+
+    override val logger: Logger = LoggerFactory.getLogger(classOf[SlickWebhookDao])
+    override val table = Tables.webhooks
+    override val lookupHookQuery = (hook: Webhook) => Tables.webhooks.filter(_.url === hook.uri.toString)
+    override val lookupKeyQuery = (uri: URI) => Tables.webhooks.filter(_.url === uri.toString)
+    override val insertHookQuery = (hook: Webhook) => Tables.webhooks += hook
+
+    def init(): Future[Unit] = db.run(Tables.webhooks.schema.createIfNotExists)
+    def all(): Future[Seq[Webhook]] = db.run(Tables.webhooks.result)
+    def allKeys(): Future[Seq[URI]] = db.run(Tables.webhooks.map(_.url).result) map {
+      _.map(new URI(_))
+    }
+  }
+
+  //noinspection TypeAnnotation
+  @Singleton
+  class SlickSlackChatDao @Inject() (val db: Database,
+                                      val databaseExecutionContext: DatabaseExecutionContext)
+    extends SlackChatHookDao with SlickHookDao[SlackChannel, SlackChatHook] {
+
+    override val logger: Logger = LoggerFactory.getLogger(classOf[SlickWebhookDao])
+    override val table = Tables.slackChatHooks
+    override val lookupHookQuery =
+      (hook: SlackChatHook) => Tables.slackChatHooks.filter(_.channel_id === hook.channel.id)
+    override val lookupKeyQuery =
+      (channel: SlackChannel) => Tables.slackChatHooks.filter(_.channel_id === channel.id)
+    override val insertHookQuery = (hook: SlackChatHook) => Tables.slackChatHooks += hook
+
+    def init(): Future[Unit] = db.run(Tables.slackChatHooks.schema.createIfNotExists)
     def all(): Future[Seq[SlackChatHook]] = db.run(Tables.slackChatHooks.result)
     def allKeys(): Future[Seq[SlackChannel]] = db.run(Tables.slackChatHooks.map(_.channel_id).result) map {
       _.map(SlackChannel)
-    }
-
-    def find(channel: SlackChannel): Future[Option[SlackChatHook]] = {
-      db.run(Tables.slackChatHooks.filter(_.channel_id === channel.id).result).map {
-        case Seq(result) => Some(result)
-        case Seq() => None
-        case _ =>
-          throw new RuntimeException(s"Multiple results returned for channel ${channel.id}")
-      }
     }
 
   }
