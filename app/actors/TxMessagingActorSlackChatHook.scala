b6d5e589d6c59ad795390457bf38d5c86e6a4b0e
Steve Phelps <sphelps@sphelps.net>
2022-06-10 11:57:54 +0100

    #49 refactored class name

--- app/actors/TxMessagingActorSlackChatHook.scala
+++ app/actors/TxMessagingActorSlackChatHook.scala
@@ -14,12 +14,12 @@ import slick.SlackChatExecutionContext
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
-class TxSlackChatHookMessagingActor @Inject()(config : Configuration, sce: SlackChatExecutionContext,
+class TxMessagingActorSlackChatHook @Inject()(config : Configuration, sce: SlackChatExecutionContext,
                                               @Assisted channel: SlackChannel) extends Actor {
 
   implicit val executionContext: SlackChatExecutionContext = sce
 
-  private val logger = LoggerFactory.getLogger(classOf[TxSlackChatHookMessagingActor])
+  private val logger = LoggerFactory.getLogger(classOf[TxMessagingActorSlackChatHook])
 
   private val slack = Slack.getInstance()
   private val methods = slack.methods(config.get[String]("slackToken"))
