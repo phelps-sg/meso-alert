e0c8befa3a1015f571381fb22406059c12707b0b
Steve Phelps <sphelps@sphelps.net>
2022-06-10 11:48:57 +0100

    #49 implemented TxSlackChatMessagingActor and refactored common components from TxWebhookMessagingActor to package object

--- app/actors/TxSlackChatMessagingActor.scala
+++ app/actors/TxSlackChatMessagingActor.scala
@@ -1,15 +1,40 @@
 package actors
 
+import com.slack.api.Slack
+import com.slack.api.methods.MethodsClient
+import com.slack.api.methods.SlackApiException
+import com.slack.api.methods.request.chat.ChatPostMessageRequest
+import com.slack.api.methods.response.chat.ChatPostMessageResponse
 import akka.actor.Actor
+import com.google.inject.Inject
+import com.google.inject.assistedinject.Assisted
+import dao.SlackChannel
 import org.slf4j.LoggerFactory
+import play.api.Configuration
+import slick.SlackChatExecutionContext
 
-class TxSlackChatMessagingActor extends Actor {
+import scala.concurrent.Future
+
+class TxSlackChatMessagingActor @Inject() (config : Configuration, sce: SlackChatExecutionContext,
+                                           @Assisted channel: SlackChannel) extends Actor {
+
+  implicit val executionContext: SlackChatExecutionContext = sce
 
   private val logger = LoggerFactory.getLogger(classOf[TxSlackChatMessagingActor])
+
+  private val slack = Slack.getInstance()
+  private val methods = slack.methods(config.get[String]("slackToken"))
+
+  def sendMessage(channelId: String, message: String): Future[ChatPostMessageResponse] = {
+    val request = ChatPostMessageRequest.builder.channel(channelId).text(message).build
+    Future {
+      methods.chatPostMessage(request)
+    }
+  }
 
   override def receive: Receive = {
     case tx: TxUpdate =>
       logger.debug("Received $tx")
-
+      sendMessage(channel.id, message(tx))
   }
 }
--- app/actors/package.scala
+++ app/actors/package.scala
@@ -60,6 +60,23 @@ package object actors {
     }
   }
 
+  val blockChairBaseURL = "https://www.blockchair.com/bitcoin"
+  def linkToTxHash(hash: String) = s"<$blockChairBaseURL/transaction/$hash|$hash>"
+  def linkToAddress(address: String) = s"<$blockChairBaseURL/address/$address|$address>"
+
+  def formatSatoshi(value: Long): String = (value / 100000000L).toString
+
+  def formatOutputAddresses(outputs: Seq[TxInputOutput]): String =
+    outputs.filterNot(_.address.isEmpty)
+      .map(output => output.address.get)
+      .distinct
+      .map(output => linkToAddress(output))
+      .mkString(", ")
+
+  def message(tx: TxUpdate): String = {
+    s"New transaction ${linkToTxHash(tx.hash)} with value ${formatSatoshi(tx.value)} BTC to " +
+      s"addresses ${formatOutputAddresses(tx.outputs)}"
+  }
   case class TxUpdate( hash: String,
                        value: Long,
                        time: DateTime,
