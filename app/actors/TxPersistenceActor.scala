a87032eb683077d531e4bf6beaa940dec60107ee
alexbrown98 <44008056+alexbrown98@users.noreply.github.com>
2022-07-05 15:22:16 +0300

    changed type of maxRetryCount from var to val

--- app/actors/TxPersistenceActor.scala
+++ app/actors/TxPersistenceActor.scala
@@ -5,8 +5,7 @@ import akka.actor.{Actor, ActorRef, PoisonPill, Props}
 import com.google.inject.Inject
 import services.MemPoolWatcherService
 import dao._
-import scala.concurrent._
-import scala.util.{Failure, Success}
+]import scala.util.{Failure, Success}
 
 
 
@@ -15,9 +14,7 @@ object TxPersistenceActor {
   trait Factory {
     def apply(out: ActorRef): Actor
   }
-    case class Die()
 
-  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
   def props(transactionUpdateDao: TransactionUpdateDao, memPoolWatcher: MemPoolWatcherService ): Props =
         Props(new TxPersistenceActor(transactionUpdateDao, memPoolWatcher))
 
@@ -26,21 +23,21 @@ object TxPersistenceActor {
 
 class TxPersistenceActor @Inject()(val transactionUpdateDao: TransactionUpdateDao,
                                   val memPoolWatcher: MemPoolWatcherService)
-                                  (implicit val ec: ExecutionContext)
-  extends Actor with TxUpdateActor with TxRetryOrDie {
+  extends Actor with TxUpdateActor with TxRetryOrDie[Int] {
 
   override val maxRetryCount = 3
-
+  override def process(tx: TxUpdate) = transactionUpdateDao.record(tx)
 
   override def preStart(): Unit = {
     super.preStart()
     registerWithWatcher()
     transactionUpdateDao.init()
   }
+  
 
   override def receive: Receive = {
-    case tx: TxUpdate => transactionUpdateDao.record(tx) onComplete {
-      case Success(_) => logger.debug(s"Successfully added tx ${tx.hash} to database.")
+    case tx: TxUpdate => process(tx) onComplete {
+      case Success(_) => logger.debug(s"Succesfuly added tx ${tx.hash} to db.")
       case Failure(ex) =>
         context.become(retryOrDie(0))
         self ! Retry(tx, ex)
@@ -50,21 +47,5 @@ class TxPersistenceActor @Inject()(val transactionUpdateDao: TransactionUpdateDa
       self ! PoisonPill
   }
 
-  override def retryOrDie(currentRetryCount: Int): Receive = {
-    case Retry(tx, _) if currentRetryCount < maxRetryCount =>
-      logger.error(s"Could not add tx ${tx.hash} to database. Retrying ...")
-      transactionUpdateDao.record(tx) onComplete {
-        case Success(_) =>
-          logger.debug(s"Successfully added tx ${tx.hash} to database.")
-          context.become(receive)
-        case Failure(ex) =>
-          context.become(retryOrDie(currentRetryCount + 1))
-          self ! Retry(tx, ex)
-      }
-    case Retry(tx, ex) if currentRetryCount >= maxRetryCount =>
-      logger.error(s"Could not add tx ${tx.hash} to database: ${ex.getMessage}")
-      context.become(receive)
-      self ! Die("could not add tx to database.")
-  }
 
 }
--- app/actors/TxRetryOrDie.scala
+++ app/actors/TxRetryOrDie.scala
@@ -1,12 +1,34 @@
 package actors
 import akka.actor._
+import scala.concurrent.Future
+import play.api.Logging
+import scala.util.{Failure, Success}
+import actors.AuthenticationActor.Die
 
-trait TxRetryOrDie extends Actor {
+
+trait TxRetryOrDie[T] extends Actor with Logging {
   val maxRetryCount: Int
+  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
 //  message types
   case class Retry(tx: TxUpdate, exception: Throwable)
 
-  def retryOrDie(currentRetryCount: Int): Receive
+  def process(tx: TxUpdate) : Future[T]
+
+  def retryOrDie(currentRetryCount: Int): Receive = {
+    case Retry(tx, _) if currentRetryCount < maxRetryCount =>
+      logger.error(s"Error processing tx ${tx.hash}. Retrying...")
+      process(tx) onComplete {
+        case Success(_) =>
+          logger.debug("Retry successfull.")
+          context.become(receive)
+        case Failure(ex) =>
+          context.become(retryOrDie(currentRetryCount + 1))
+          self ! Retry(tx, ex)
+      }
+    case Retry(tx, ex) if currentRetryCount >= maxRetryCount =>
+      context.become(receive)
+      self ! Die(s"Could not proccess tx ${tx.hash}. $ex")
+  }
 
 }
