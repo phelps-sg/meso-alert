c48717ce2d134cbe3796a1b2fc5712ebd068b1df
Steve Phelps <sphelps@sphelps.net>
2022-07-07 15:29:29 +0100

    #76 initialise sodium in key gen utility to ensure sufficient entropy

--- app/util/GenerateSodiumKey.scala
+++ app/util/GenerateSodiumKey.scala
@@ -1,10 +1,15 @@
 package util
 
+import org.abstractj.kalium.NaCl
 import org.abstractj.kalium.crypto.Random
 
 object GenerateSodiumKey {
 
   def main(args: Array[String]): Unit = {
+    val result = NaCl.init()
+    if (result < 0) {
+      throw new RuntimeException(s"Could not initialise sodium library: $result")
+    }
     val rng = new Random()
     val encoder = java.util.Base64.getEncoder
     val key = encoder.encode(rng.randomBytes(32)).map(_.toChar).mkString
