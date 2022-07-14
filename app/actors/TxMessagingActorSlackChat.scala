050976dbb2f60212f4712dd9b242f0b20a0cf372
alexbrown98 <44008056+alexbrown98@users.noreply.github.com>
2022-07-14 18:57:32 +0300

    #64 scalafix

--- app/actors/TxMessagingActorSlackChat.scala
+++ app/actors/TxMessagingActorSlackChat.scala
@@ -6,7 +6,6 @@ import com.google.inject.Inject
 import com.google.inject.assistedinject.Assisted
 import com.slack.api.methods.AsyncMethodsClient
 import com.slack.api.methods.request.chat.ChatPostMessageRequest
-import com.slack.api.methods.response.chat.ChatPostMessageResponse
 import dao.SlackChatHook
 import play.api.{Configuration, Logging}
 import slack.SlackClient
@@ -14,7 +13,7 @@ import slick.SlackChatExecutionContext
 
 import scala.concurrent.Future
 import scala.jdk.FutureConverters._
-import scala.util.{Failure, Random, Success}
+import scala.util.Random
 
 object TxMessagingActorSlackChat  {
 
@@ -36,7 +35,7 @@ class TxMessagingActorSlackChat @Inject()(protected val config : Configuration, 
 
   implicit val ec: SlackChatExecutionContext = sce
 
-  override def success() = logger.info("Succesfully posted message")
+  override def success(): Unit = logger.info("Succesfully posted message")
   override val maxRetryCount = 3
 
   override def process(tx: TxUpdate): Future[Any] = {
