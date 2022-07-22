4ece53600d05c5be9d2fabce8c61eb1e683104fc
Steve Phelps <sphelps@sphelps.net>
2022-07-22 18:52:07 +0100

    #103 - ensure all values are quoted to allow for special characters in URL values

--- docker/project-setup.sh
+++ docker/project-setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 cat > /root/meso-alert-config.sh << EOF
-export POSTGRES_PASSWORD=$1
-export POSTGRES_PORT=$2
-export SODIUM_KEY=$3
-export PLAY_KEY=$4
-export SLACK_CLIENT_ID=$5
-export SLACK_CLIENT_SECRET=$6
-export POSTGRES_HOST=$7
-export EMAIL_SMTP_HOST=$8
-export EMAIL_SMTP_PORT=$9
-export EMAIL_HOST=${10}
-export EMAIL_HOST_PASSWORD=${11}
-export EMAIL_DESTINATION=${12}
-export SLACK_DEPLOY_URL=${13}
+export POSTGRES_PASSWORD='$1'
+export POSTGRES_PORT='$2'
+export SODIUM_KEY='$3'
+export PLAY_KEY='$4'
+export SLACK_CLIENT_ID='$5'
+export SLACK_CLIENT_SECRET='$6'
+export POSTGRES_HOST='$7'
+export EMAIL_SMTP_HOST='$8'
+export EMAIL_SMTP_PORT='$9'
+export EMAIL_HOST='${10}'
+export EMAIL_HOST_PASSWORD='${11}'
+export EMAIL_DESTINATION='${12}'
+export SLACK_DEPLOY_URL='${13}'
 
 EOF
