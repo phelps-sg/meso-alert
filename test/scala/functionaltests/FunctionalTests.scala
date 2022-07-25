21ead7b697f9a580ed04bbbaabc403d0e5155571
Steve Phelps <sphelps@sphelps.net>
2022-07-25 06:53:22 +0100

    #54 - define base URL as val

--- test/scala/functionaltests/FunctionalTests.scala
+++ test/scala/functionaltests/FunctionalTests.scala
@@ -10,8 +10,10 @@ class FunctionalTests extends flatspec.AnyFlatSpec with should.Matchers with Web
   private val options = new FirefoxOptions().setHeadless(true)
   implicit val webDriver: FirefoxDriver = new FirefoxDriver(options)
 
+  val stagingURL: String = "https://f34d1cfcb2d9.eu.ngrok.io/"
+
   "The home page" should "render" in {
-    go to "https://f34d1cfcb2d9.eu.ngrok.io/"
+    go to stagingURL
     pageTitle should be("Block Insights - Access free real-time mempool data")
   }
 
