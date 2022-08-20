d6aa52ff4315b474972f8719ee10127f6be08319
Steve Phelps <sphelps@sphelps.net>
2022-08-20 10:21:18 +0100

    formatting

--- app/controllers/WebhooksController.scala
+++ app/controllers/WebhooksController.scala
@@ -3,7 +3,13 @@ package controllers
 import akka.actor.ActorSystem
 import dao.Webhook
 import play.api.libs.json._
-import play.api.mvc.{Action, ActionBuilder, BaseController, ControllerComponents, Result}
+import play.api.mvc.{
+  Action,
+  ActionBuilder,
+  BaseController,
+  ControllerComponents,
+  Result
+}
 import services.HooksManagerWebService
 
 import java.net.URI
