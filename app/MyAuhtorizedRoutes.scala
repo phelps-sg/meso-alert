ab54b3dd64627b0602d9b760b7af7795132a0608
Steve Phelps <sphelps@sphelps.net>
2022-08-25 10:26:23 +0100

    scalafmt

--- app/MyAuhtorizedRoutes.scala
+++ app/MyAuhtorizedRoutes.scala
@@ -1,13 +1,23 @@
 import javax.inject.Inject
 
-import be.objectify.deadbolt.scala.filters.{AuthorizedRoute, AuthorizedRoutes, FilterConstraints}
+import be.objectify.deadbolt.scala.filters.{
+  AuthorizedRoute,
+  AuthorizedRoutes,
+  FilterConstraints
+}
 import be.objectify.deadbolt.scala.filters._
 
-class MyAuthorizedRoutes @Inject() (filterConstraints: FilterConstraints) extends AuthorizedRoutes {
+class MyAuthorizedRoutes @Inject() (filterConstraints: FilterConstraints)
+    extends AuthorizedRoutes {
 
   override val routes: Seq[AuthorizedRoute] =
     Seq(
-      AuthorizedRoute(Get, "/", filterConstraints.subjectNotPresent, handler = None)
+      AuthorizedRoute(
+        Get,
+        "/",
+        filterConstraints.subjectNotPresent,
+        handler = None
+      )
     )
 
 }
