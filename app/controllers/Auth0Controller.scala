45a81cb163e93dc5cbf4e87d434d46ca78c46825
Steve Phelps <sphelps@sphelps.net>
2022-08-29 17:13:21 +0100

    #4 obtain actual secret from with secret service instead of dummy value

--- app/controllers/Auth0Controller.scala
+++ app/controllers/Auth0Controller.scala
@@ -1,17 +1,30 @@
 package controllers
 
+import dao.{Secret, UserId}
 import play.api.Configuration
 import play.api.libs.json.{Json, Writes}
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import services.SlackSecretsManagerService
 import sttp.model.Uri
 import util.ConfigLoaders.UriConfigLoader
+import util.Encodings.base64Encode
 
 import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
 
 class Auth0Controller @Inject() (
+    val slackSecretsManagerService: SlackSecretsManagerService,
     val controllerComponents: ControllerComponents,
     protected val config: Configuration
-) extends BaseController {
+)(implicit ec: ExecutionContext) extends BaseController {
+
+  val encoder = java.util.Base64.getEncoder
+
+  val auth0Configuration: Auth0Configuration = Auth0Configuration(
+    config.get[String]("auth0.clientId"),
+    config.get[Uri]("auth0.domain"),
+    config.get[Uri]("auth0.audience")
+  )
 
   implicit val auth0ConfigurationWrites: Writes[Auth0Configuration] =
     (config: Auth0Configuration) =>
@@ -21,23 +34,31 @@ class Auth0Controller @Inject() (
         "audience" -> config.audience.toString()
       )
 
-  case class Auth0Configuration(clientId: String, domain: Uri, audience: Uri)
+  implicit val resultWrites: Writes[Result] =
+    (result: Result) =>
+      Json.obj(
+        "userId" -> result.userId.id,
+        "secret" -> base64Encode(result.secret.data),
+        "slackClientId" -> result.slackClientId
+      )
 
-  val auth0Configuration: Auth0Configuration = Auth0Configuration(
-    config.get[String]("auth0.clientId"),
-    config.get[Uri]("auth0.domain"),
-    config.get[Uri]("auth0.audience")
-  )
+  case class Auth0Configuration(clientId: String, domain: Uri, audience: Uri)
+  case class Result(userId: UserId, secret: Secret, slackClientId: String)
 
   def configuration(): Action[AnyContent] = Action { _ =>
     Ok(Json.toJson(auth0Configuration))
   }
 
-  def secret(userId: Option[String]): Action[AnyContent] = Action { _ =>
-    userId match {
-      case Some(user) => Ok(s"secret for ${user}")
-      case None       => Ok("user is not logged in")
+  def secret(uid: Option[String]): Action[AnyContent] = Action.async { _ =>
+    uid match {
+
+      case Some(identifier) =>
+        val userId = UserId(identifier)
+        slackSecretsManagerService.generateSecret(userId) map { secret =>
+          Ok(Json.toJson(Result(userId, secret, "TODO")))
+        }
+
+      case None       => Future { ServiceUnavailable("user is not logged in") }
     }
-  // TODO: Implement this
   }
 }
