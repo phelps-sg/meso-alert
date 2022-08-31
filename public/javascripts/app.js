3b7ff5e37f600afe4aeb388ed817634878e65d41
alexbrown98 <44008056+alexbrown98@users.noreply.github.com>
2022-08-31 11:38:38 +0100

    user is not logged out on page reload

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -54,7 +54,8 @@ const configureClient = async () => {
   auth0 = await createAuth0Client({
     domain: config.domain,
     client_id: config.clientId,
-    audience: config.audience
+    audience: config.audience,
+    cacheLocation: 'localstorage'
   });
 };
 
