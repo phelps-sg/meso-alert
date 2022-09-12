5f30445d877d077482affacff0796c2e98811ab5
Steve Phelps <sphelps@sphelps.net>
2022-09-12 14:50:15 +0100

    only send backup notification on failure

--- docker/postgres-backup.sh
+++ docker/postgres-backup.sh
@@ -21,7 +21,8 @@ notification_msg="$BACKUP_APP: postgres backup of $file_name to $S3_BUCKET FAILE
 if pg_dump --no-password -h $PG_HOST -p $PG_PORT -U $PG_USER --format=tar $PG_DATABASE | gzip -c >"$file_name"; then
   if aws s3 cp "$file_name" "$S3_BUCKET"; then
     echo "$BACKUP_APP: postgres backup of $file_name to $S3_BUCKET SUCCESS"
-  else
-    send_slack_notification "$notification_msg"
+    exit 0
   fi
 fi
+
+send_slack_notification "$notification_msg"
