507a7747fea35b174dd3bd0dc0673d47f7148c5d
Steve Phelps <sphelps@sphelps.net>
2022-09-12 13:57:05 +0100

    only send backup notification on failure

--- docker/postgres-backup.sh
+++ docker/postgres-backup.sh
@@ -16,12 +16,12 @@ chmod 0600 /root/.pgpass
 
 file_name="pg-backup/pg-backup-$date1.tar.gz"
 
-notification_msg="block-insights: postgres backup of $file_name to $S3_BUCKET FAILED"
+notification_msg="$BACKUP_APP: postgres backup of $file_name to $S3_BUCKET FAILED"
 
 if pg_dump --no-password -h $PG_HOST -p $PG_PORT -U $PG_USER --format=tar $PG_DATABASE | gzip -c >"$file_name"; then
   if aws s3 cp "$file_name" "$S3_BUCKET"; then
-    notification_msg="$BACKUP_APP: postgres backup of $file_name to $S3_BUCKET SUCCESS"
+    echo "$BACKUP_APP: postgres backup of $file_name to $S3_BUCKET SUCCESS"
+  else
+    send_slack_notification "$notification_msg"
   fi
 fi
-
-send_slack_notification "$notification_msg"
