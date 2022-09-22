296f59296ad3c3e77cb080dc54f283a6e6388eb9
Steve Phelps <sphelps@sphelps.net>
2022-09-22 14:52:16 +0100

    #189 - BlockChainWatcherActor has message to register a listener to receive notifications about transactions included in blocks

--- app/actors/BlockChainWatcherActor.scala
+++ app/actors/BlockChainWatcherActor.scala
@@ -1,27 +1,38 @@
 package actors
 
 import actors.MessageHandlers.UnrecognizedMessageHandlerFatal
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, ActorRef, Props}
 import com.google.inject.Inject
 import org.bitcoinj.core.StoredBlock
+import org.bitcoinj.params.AbstractBitcoinNetParams
 import play.api.Logging
-import services.BlockChainProvider
+import services.{BlockChainProvider, NetParamsProvider}
 
 object BlockChainWatcherActor {
-  final case class NewBlock(block: StoredBlock)
 
-  def props(blockChainProvider: BlockChainProvider): Props = Props(
-    new BlockChainWatcherActor(blockChainProvider)
+  final case class NewBlock(block: StoredBlock)
+  final case class WatchTxConfidence(hash: TxHash, listener: ActorRef)
+
+  def props(
+      blockChainProvider: BlockChainProvider,
+      netParamsProvider: NetParamsProvider
+  ): Props = Props(
+    new BlockChainWatcherActor(blockChainProvider, netParamsProvider)
   )
 }
 
 class BlockChainWatcherActor @Inject() (
-    val blockChainProvider: BlockChainProvider
+    protected val blockChainProvider: BlockChainProvider,
+    protected val netParamsProvider: NetParamsProvider
 ) extends Actor
     with Logging
     with UnrecognizedMessageHandlerFatal {
 
   import actors.BlockChainWatcherActor._
+
+  implicit val params: AbstractBitcoinNetParams = netParamsProvider.get
+
+  protected var txWatchers: Map[TxHash, ActorRef] = Map()
 
   blockChainProvider.get.addNewBestBlockListener((block: StoredBlock) =>
     self ! NewBlock(block)
@@ -33,6 +44,14 @@ class BlockChainWatcherActor @Inject() (
       logger.info(
         s"New best block ${block.getHeader.getHash} with height ${block.getHeight}"
       )
+      block.getHeader.getTransactions.forEach { tr =>
+        txWatchers.get(TxHash(tr)) map { listener =>
+          listener ! TxUpdate(tr)
+        }
+      }
+
+    case WatchTxConfidence(hash: TxHash, listener) =>
+      txWatchers += hash -> listener
 
     case other =>
       unrecognizedMessage(other)
