daaec8f0a180c3b661756d63e22232eb97b9e678
Steve Phelps <sphelps@sphelps.net>
2022-12-15 15:55:53 +0000

    formatting

--- test/scala/unittests/ActorTests.scala
+++ test/scala/unittests/ActorTests.scala
@@ -762,26 +762,33 @@ class ActorTests
     }
 
     "forward separate messages when they are separated by a long delay" in new TestFixtures {
+
       advanceClockTo(2000)
       actor ! tx
       expectTx(tx)
+
       advanceClockTo(4000)
       actor ! tx1
       expectTx(tx1)
     }
 
     "forward a batch of messages when they are separated by a short delay" in new TestFixtures {
+
       advanceClockTo(500)
       actor ! tx
       expectTx(tx)
+
       advanceClockTo(600)
       actor ! tx1
       expectNoMessage()
+
       advanceClockTo(700)
       actor ! tx2
+
       advanceClockTo(3000)
       actor ! tx3
       expectTxs(Vector(tx1, tx2, tx3))
+
       advanceClockTo(5000)
       actor ! tx4
       expectTx(tx4)
